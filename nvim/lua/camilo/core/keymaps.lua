f25e6b997b1518f9f7198829939417aca618ea14
Samuel Camilo <itscamilo@outlook.com>
2024-04-21 02:36:52 -0300

    feat: added basic configurations

--- nvim/lua/camilo/core/keymaps.lua
+++ nvim/lua/camilo/core/keymaps.lua
@@ -11,10 +11,12 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incremen
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+-- keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+-- keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+-- keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+-- keymap.set("n", "<leader>sn", "<C-w>h", { desc = "Go to next split window" })
+-- keymap.set("n", "<leader>sp", "<C-w>l", { desc = "Go to previous split window" })
+-- keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
--- nvim/lua/camilo/plugins/init.lua
+++ nvim/lua/camilo/plugins/init.lua
@@ -3,4 +3,3 @@ return {
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
   "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 }
-
