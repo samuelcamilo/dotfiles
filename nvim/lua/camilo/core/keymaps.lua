d1a2741daa324b6b679e29828fe7ee5b0b59ce3d
Samuel Camilo <itscamilo@outlook.com>
2024-06-25 21:35:05 -0300

    feat: updated files

--- nvim/lua/camilo/core/keymaps.lua
+++ nvim/lua/camilo/core/keymaps.lua
@@ -3,12 +3,12 @@ vim.g.mapleader = " " -- set leader key to space
 local keymap = vim.keymap -- for conciseness
 
 -- general keymaps
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" }) -- use 'jk' to exit insert mode
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" }) -- clear search highlights
+-- keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" }) -- use 'jk' to exit insert mode
+-- keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" }) -- clear search highlights
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+-- keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+-- keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
 -- keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
@@ -18,8 +18,11 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 -- keymap.set("n", "<leader>sp", "<C-w>l", { desc = "Go to previous split window" })
 -- keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+-- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc mm= "Close current tab" }) -- close current tab
+-- keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+-- keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+-- keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+keymap.set("n", "<leader>e", "", { desc = "Explorer Options" }) -- Show Telescope Options
+keymap.set("n", "<leader>f", "", { desc = "Find Options (Telescope)" }) -- Show Telescope Options
