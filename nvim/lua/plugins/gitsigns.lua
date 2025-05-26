2da4f73abb912aea485b76d0cfcd5469608a698e
Samuel Camilo <sc4milo@gmail.com>
2025-05-26 11:45:59 -0300

    feat: alpha plugin was adde

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,21 +2,27 @@
 -- Enhances version control workflow with visual diff indicators and customizable symbols.
 
 return {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-        signs = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
-        },
-        signs_staged = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
-        },
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
     },
+    signs_staged = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  },
+  config = function()
+    require('gitsigns').setup()
+
+    vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+    vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
+  end,
 }
